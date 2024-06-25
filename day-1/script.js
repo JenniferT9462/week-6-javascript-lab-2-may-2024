21dce4705b3cd9a53e1255fce99403096d72cb94
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-06-24 21:35:37 -0500

    Completed Day 1

--- day-1/script.js
+++ day-1/script.js
@@ -12,7 +12,7 @@ function partOneA () {
   billNumber = Number(bill);
 
   // 3. calculate tip
-  tip = bill * .10;
+  tip = billNumber * .10;
 
   // 4. set the message
   messageParagraph.innerHTML = `Your bill is ${bill}. Your tip is ${tip} at 10%.`
@@ -22,17 +22,21 @@ function partOneA () {
 // Try It!
 // Now try it at 20%
 function partOneB () {
-  alert("Try it!");
+  // alert("Try it!");
   // Tip calulator at 20%
   let bill, billNumber, tip;
 
   // 1. prompt for the bill
-  
+  bill = prompt("What is the amount of your bill?");
+
   // 2. convert it to number
+  billNumber = Number(bill);
   
   // 3. calculate tip
+  tip = billNumber * .20;
 
   // 4. set the message
+  messageParagraph.innerHTML = `Your bill is ${bill}. Your tip is ${tip} at 20%.`;
   
 }
 
@@ -53,15 +57,20 @@ function partTwoA () {
 }
 
 // Try it!
+//Split the bill between 4 people
 function partTwoB () {
-  alert("Try it!");
+  let bill, billNumber, split;
   // 1. prompt for the bill
+  bill = prompt("What is the amount of your bill?");
+
 
   // 2. convert it to number
-
+  billNumber = Number(bill);
   // 3. calculate split bill
+  split = billNumber/4;
 
   // 4. set the message
+  messageParagraph.innerHTML = `Your bill is $${bill}. Your part of the bill is $${split} among 4 friends.`;
 }
 
 // Example
@@ -72,7 +81,7 @@ function tenPercentTip(x) {
 }
 // This is function that takes the tip and bill as arguments and creates a message for the user about what should be tipped.
 function tipMessage(tip, bill, tipRate) {
-  return `Your bill is ${bill}. You should tip $${tip}  at ${tipRate} percent.`
+  return `Your bill is ${bill}. Your should tip $${tip}  at ${tipRate} percent.`
 }
 function partThreeA () {
   let bill, billNumber, tip, message;
@@ -91,19 +100,23 @@ function partThreeA () {
 // Try it!
 // This is a function that takes a number, x as an argument and returns the amount you shuold tip at 20%
 function twentyPercentTip(x) {
+  return x*.20
   
 }
 function partThreeB () {
-  alert("Try it!");
+  // alert("Try it!");
   let bill, billNumber, tip, message;
   // 1. prompt the user for the bill
+  bill = prompt("What is your bill amount?");
   
   // 2. convert the bill to a number
-  
+  billNumber = Number(bill);
   // 3. calculate the tip
+  tip = twentyPercentTip(billNumber);
   
   // 4. create the message
   // HINT: use tipMessage, which is defined above partThreeA
+  message = tipMessage(tip, bill, 20);
   
   // 5. set the message
   messageParagraph.innerHTML = message;
@@ -116,7 +129,7 @@ function splitBillByThree(x) {
   return x/3;
 }
 function messageForSplitBill(bill, split, count) {
-  return `Your bill is ${bill}. You part of the bill is ${split} among ${count} friends.`
+  return `Your bill is $${bill}. You part of the bill is $${split} among ${count} friends.`
 }
 function partFourA () {
   let bill, billNumber, split, message;
@@ -137,24 +150,29 @@ function partFourA () {
 function splitBillByFour(x) {
   // Try it! return x divided by 4
   // to calculate the bill.
+  return x/4;
 }
 
 function partFourB () {
-  alert("Try it!");
+  // alert("Try it!");
   let bill, billNumber, split, message;
   // 1. prompt for bill
+  bill = prompt("What is your bill amount?");
 
   // 2. convert bill to number 
+  billNumber = Number(bill);
 
   // 3. use a function to calculate the split
+  split = splitBillByFour(billNumber);
 
   // 4. use a function to create the message
+  message = messageForSplitBill(bill, split, "four");
 
   messageParagraph.innerHTML = message;
 }
 
 
-// Bonus: Create a splitBillByN function that will take two arguements, the bill and n. N is the number of friends splitting.
+// Bonus: Create a splitBillByN function that will take two arguments, the bill and n. N is the number of friends splitting.
 // Then use the splitBillByN in part 4 A and B instead of splitBillByThree and splitBillByFour.
 // Think about how functions and arguments can make your code more flexible and ready to take on new requirements. (For example, can you make it split bill by 8 or 10 friends? How does splitBillByN help?)
 
